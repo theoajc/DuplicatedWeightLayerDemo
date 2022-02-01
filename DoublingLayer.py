02a503288b11f05ccc2d21fadca52166629ce7c0
Theoajc <Theoajclarke@outlook.com>
2022-02-01 19:36:24 +0000

    layer copying and repeating every element

--- DoublingLayer.py
+++ DoublingLayer.py
@@ -5,6 +5,80 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plt
 from torch.nn.modules.module import Module
+from torch import Tensor
+from torch.nn import Linear
+from torch.nn.parameter import Parameter
+
+
+class DuplicateLayer(Module):
+    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
+    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        bias: If set to ``False``, the layer will not learn an additive bias.
+            Default: ``True``
+    Shape:
+        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
+          dimensions including none and :math:`H_{in} = \text{in\_features}`.
+        - Output: :math:`(*, H_{out})` where all but the last dimension
+          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
+    Attributes:
+        weight: the learnable weights of the module of shape
+            :math:`(\text{out\_features}, \text{in\_features})`. The values are
+            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
+            :math:`k = \frac{1}{\text{in\_features}}`
+        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
+                If :attr:`bias` is ``True``, the values are initialized from
+                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
+                :math:`k = \frac{1}{\text{in\_features}}`
+    Examples::
+        >>> m = nn.Linear(20, 30)
+        >>> input = torch.randn(128, 20)
+        >>> output = m(input)
+        >>> print(output.size())
+        torch.Size([128, 30])
+    """
+    copied_layer: nn.Linear
+
+    def __init__(self, copied_layer=None, device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(DuplicateLayer, self).__init__()
+        self.in_features = copied_layer.in_features
+        self.out_features = copied_layer.out_features * 2
+        memo_weight = {}
+        memo_bias = {}
+        copied_layer_weight = copied_layer.weight.__deepcopy__(memo_weight)
+        copied_layer_bias = copied_layer.bias.__deepcopy__(memo_bias)
+        copied_layer_weight_repeated = copied_layer_weight.repeat_interleave(2)
+        copied_layer_bias_repeated = copied_layer_bias.repeat_interleave(2)
+        print(copied_layer.weight)
+        print(copied_layer_weight_repeated)
+        self.weight = copied_layer_weight
+        if copied_layer_bias is not None:
+            self.bias = copied_layer_bias
+        else:
+            self.register_parameter('bias', None)
+        # self.reset_parameters()
+
+    # def reset_parameters(self) -> None:
+    #     # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
+    #     # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
+    #     # https://github.com/pytorch/pytorch/issues/57109
+    #     init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+    #     if self.bias is not None:
+    #         fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
+    #         bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+    #         init.uniform_(self.bias, -bound, bound)
+
+    def forward(self, input: Tensor) -> Tensor:
+        return F.linear(input, self.weight, self.bias)
+
+    def extra_repr(self) -> str:
+        return 'in_features={}, out_features={}, bias={}'.format(
+            self.in_features, self.out_features, self.bias is not None
+        )
+
 
 # Download training data from open datasets.
 training_data = datasets.FashionMNIST(
@@ -51,6 +125,7 @@ class NeuralNetwork(nn.Module):
             nn.ReLU(),
             nn.Linear(4092, 10)
         )
+        duplicate = DuplicateLayer(self.linear_relu_stack[2])
 
     def forward(self, x):
         x = self.flatten(x)
@@ -106,65 +181,3 @@ for t in range(epochs):
     test(test_dataloader, model, loss_fn)
 print("Done!")  
 
-
-class Linear(Module):
-    r"""Applies a linear transformation to the incoming data: :math:`y = xA^T + b`
-    This module supports :ref:`TensorFloat32<tf32_on_ampere>`.
-    Args:
-        in_features: size of each input sample
-        out_features: size of each output sample
-        bias: If set to ``False``, the layer will not learn an additive bias.
-            Default: ``True``
-    Shape:
-        - Input: :math:`(*, H_{in})` where :math:`*` means any number of
-          dimensions including none and :math:`H_{in} = \text{in\_features}`.
-        - Output: :math:`(*, H_{out})` where all but the last dimension
-          are the same shape as the input and :math:`H_{out} = \text{out\_features}`.
-    Attributes:
-        weight: the learnable weights of the module of shape
-            :math:`(\text{out\_features}, \text{in\_features})`. The values are
-            initialized from :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})`, where
-            :math:`k = \frac{1}{\text{in\_features}}`
-        bias:   the learnable bias of the module of shape :math:`(\text{out\_features})`.
-                If :attr:`bias` is ``True``, the values are initialized from
-                :math:`\mathcal{U}(-\sqrt{k}, \sqrt{k})` where
-                :math:`k = \frac{1}{\text{in\_features}}`
-    Examples::
-        >>> m = nn.Linear(20, 30)
-        >>> input = torch.randn(128, 20)
-        >>> output = m(input)
-        >>> print(output.size())
-        torch.Size([128, 30])
-    """
-    copied_layer: nn.Linear
-
-    def __init__(self, copied_layer
-                 device=None, dtype=None) -> None:
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(Linear, self).__init__()
-        self.in_features = copied_layer.in_features
-        self.out_features = copied_layer.out_features * 2
-        self.weight = Parameter(torch.empty((out_features, in_features), **factory_kwargs))
-        if bias:
-            self.bias = Parameter(torch.empty(out_features, **factory_kwargs))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
-
-    def reset_parameters(self) -> None:
-        # Setting a=sqrt(5) in kaiming_uniform is the same as initializing with
-        # uniform(-1/sqrt(in_features), 1/sqrt(in_features)). For details, see
-        # https://github.com/pytorch/pytorch/issues/57109
-        init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        if self.bias is not None:
-            fan_in, _ = init._calculate_fan_in_and_fan_out(self.weight)
-            bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-            init.uniform_(self.bias, -bound, bound)
-
-    def forward(self, input: Tensor) -> Tensor:
-        return F.linear(input, self.weight, self.bias)
-
-    def extra_repr(self) -> str:
-        return 'in_features={}, out_features={}, bias={}'.format(
-            self.in_features, self.out_features, self.bias is not None
-        )
